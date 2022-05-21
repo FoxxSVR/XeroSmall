86f67a1316af11c1cd8534ea43f23d4bb73d9706
Foxx Suzuki <begreflex@gmail.com>
2022-05-21 16:32:14 -0400

    Public

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -18,43 +18,9 @@ namespace Xero
         private static ApiAvatar a;
         private static VRC.UI.Elements.QuickMenu _quickMenuInstance;
         private static SelectedUserMenuQM _selectedUserLocal;
+        private static byte[] _imageByteArray;
+        private static Sprite _createdSprite;
 
-        public static Sprite LoadSpriteFromDisk(this string path)
-        {
-            if (string.IsNullOrEmpty(path))
-                return null;
-
-            byte[] array = File.ReadAllBytes(path);
-            if (array == null || array.Length == 0)
-                return null;
-
-            Texture2D texture2D = new Texture2D(512, 512);
-            if (!Il2CppImageConversionManager.LoadImage(texture2D, array))
-                return null;
-
-            Sprite sprite = Sprite.CreateSprite(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f), 100f, 0U, 0, default(Vector4), false);
-            sprite.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-            return sprite;
-        }
-
-        public static void LoadSpriteFromUrl(this Image Instance, string url)
-        {
-            if (string.IsNullOrEmpty(url))
-                return;
-
-            byte[] array = new WebClient().DownloadData(url);
-            if (array == null || array.Length == 0)
-                return;
-
-            Texture2D texture2D = new Texture2D(512, 512);
-            if (!Il2CppImageConversionManager.LoadImage(texture2D, array))
-                return;
-
-            Sprite sprite = Sprite.CreateSprite(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f), 100f, 0U, 0, default(Vector4), false);
-            sprite.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-            Instance.sprite = sprite;
-            Instance.color = Color.white;
-        }
         public static void DelegateSafeInvoke(this Delegate @delegate, params object[] args)
         {
             Delegate[] invocationList = @delegate.GetInvocationList();
